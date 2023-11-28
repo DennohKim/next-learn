c9a6668d0ac4a2fc916311409ff4b8468ac42688
DennohKim <kimathidennis38@gmail.com>
2023-11-28 08:42:44 +0300

    next font

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Lexend, Lusitana } from 'next/font/google';
+ 
+export const lexend = Lexend({ subsets: ['latin'] });
+export const lusitana = Lusitana({   weight: ['400', '700'],
+subsets: ['latin']  });
