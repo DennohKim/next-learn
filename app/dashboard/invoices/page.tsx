a1eb6a43c1eafb0bcd976d26ff45c2feaf1bc318
DennohKim <kimathidennis38@gmail.com>
2023-11-29 10:29:08 +0300

    Add metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -8,6 +8,12 @@ import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
+
 
 export default async function Page({
   searchParams,
